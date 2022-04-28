185c70ab87a624bdbb55777a77fb2cc7b50c924b
edgaras <stasiukaitisedgaras87@gmail.com>
2022-04-28 23:52:26 +0200

    Overriding the endpoint authorization configuration

--- project_1/src/main/java/edge/security/config/ProjectConfig.java
+++ project_1/src/main/java/edge/security/config/ProjectConfig.java
@@ -2,6 +2,9 @@ package edge.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.httpBasic(); // by default, app uses HTTP Basic authentication
+		http.authorizeRequests()
+				.anyRequest()        // all requests(endpoints)
+					.authenticated() // require authentication
+					//.permitAll()   // accessible without credentials 
+					
+					
+		;
+	}
 
 	@Bean
 	public UserDetailsService userDetailsService() {
+
 		InMemoryUserDetailsManager userDetailsService =  new InMemoryUserDetailsManager();
 		UserDetails user = User.withUsername("jon")
 				        .password("pass")
@@ -28,5 +47,7 @@ public class ProjectConfig {
 	public PasswordEncoder encoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+
 	
 }
