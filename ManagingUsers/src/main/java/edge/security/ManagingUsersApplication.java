7f96b8c6260c9df4f46f301383a4db82fc33885f
edgaras <stasiukaitisedgaras87@gmail.com>
2022-04-29 09:03:53 +0200

    Initialize poject

---
+++ ManagingUsers/src/main/java/edge/security/ManagingUsersApplication.java
@@ -0,0 +1,13 @@
+package edge.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingUsersApplication.class, args);
+	}
+
+}
---
+++ ManagingUsers/src/test/java/edge/security/ManagingUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package edge.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
