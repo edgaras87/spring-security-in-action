70c32aba9cf79fd24c54017d062b2222bcb6824a
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-03 09:57:06 +0200

    Cryptography examples

--- PasswordEncoder/src/main/java/edge/security/PasswordEncoderApplication.java
+++ PasswordEncoder/src/main/java/edge/security/PasswordEncoderApplication.java
@@ -1,7 +1,9 @@
 package edge.security;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PasswordEncoderApplication {
@@ -9,5 +11,14 @@ public class PasswordEncoderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordEncoderApplication.class, args);
 	}
+	
+	@Bean
+	ApplicationRunner cryptographyExamples() {
+		return args -> {
+			CryptographyExamples.generateKeyExamples();
+			CryptographyExamples.encryptorsExamples();
+		};
+	}
+	
 
 }
