221db3ee7ece77509584afe0a4d1bbde45cda236
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-04 08:30:11 +0200

    Customize the http response for a failed authentication

--- ImplementingAuthentication/src/main/java/edge/authentication/security/config/ProjectConfig.java
+++ ImplementingAuthentication/src/main/java/edge/authentication/security/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -18,6 +19,16 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 		this.authenticationProvider = authenticationProvider;
 	}
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic(c -> {
+				c.realmName("other");
+				c.authenticationEntryPoint(new CustomEntryPoint());		
+		});
+		
+		http.authorizeHttpRequests().anyRequest().authenticated();
+	}
+
 	/**
 	 * Works fine without this configuration !!!
 	 */
