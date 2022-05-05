b8ef09b54ba0906828c62127de0b8662e24bf55c
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-05 18:25:56 +0200

    Initialize configuring authorization project

---
+++ ConfiguringAuthorization/src/main/java/edge/authorization/ConfiguringAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package edge.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguringAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguringAuthorizationApplication.class, args);
+	}
+
+}
