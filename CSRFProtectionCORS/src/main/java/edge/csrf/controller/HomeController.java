a6d079e6be39a1d025a3e1d8e8c5d5d1226ebb41
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-09 18:14:14 +0200

    Turning off CSRF protection on particular request path using matchers

--- CSRFProtectionCORS/src/main/java/edge/csrf/controller/HomeController.java
+++ CSRFProtectionCORS/src/main/java/edge/csrf/controller/HomeController.java
@@ -6,17 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/home")
+@RequestMapping()
 public class HomeController {
 
-	@GetMapping
+	@GetMapping(path = "/home")
 	public String homeGet() {
 		return "get home works!!!";
 	}
 	
-	@PostMapping
+	@PostMapping(path = "/home")
 	public String homePost() {
 		return "post home works!!!";
+	}
+	
+	@PostMapping(path = "/ciao")
+	public String ciaoPost() {
+		return "post ciao works!!!";
+	}
+	
+	@PostMapping(path = "/hola")
+	public String holaPost() {
+		return "post hola works!!!";
 	}
 	
 }
--- CSRFProtectionCORS/src/main/java/edge/csrf/security/ProjectConfig.java
+++ CSRFProtectionCORS/src/main/java/edge/csrf/security/ProjectConfig.java
@@ -2,12 +2,16 @@ package edge.csrf.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import edge.csrf.security.filter.CsrfTokenLogger;
 
@@ -31,7 +35,20 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 		http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
 			.authorizeRequests()
 				.anyRequest().authenticated();
-		//http.csrf().disable();
+		http.csrf(c -> {
+			// 1. antMatcher
+			c.ignoringAntMatchers("/ciao");
+			
+			// 2. mvcMatcher
+			HandlerMappingIntrospector i = new HandlerMappingIntrospector();
+			MvcRequestMatcher m = new MvcRequestMatcher(i, "/hola");
+			c.ignoringRequestMatchers(m);
+			
+			// 3. regexMatcher
+			String pattern = ".*[0-9].*";
+			String httpMethod = HttpMethod.POST.toString();
+			RegexRequestMatcher r = new RegexRequestMatcher(pattern, httpMethod);
+		});
 	}
 
 	@Bean
