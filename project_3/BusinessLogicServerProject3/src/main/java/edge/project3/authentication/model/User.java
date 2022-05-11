5ca98d8fd9b2b0d2d7997f4b89463a07dacbb19a
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-11 08:33:17 +0200

    Implement the proxy to the authentication server

---
+++ project_3/BusinessLogicServerProject3/src/main/java/edge/project3/authentication/model/User.java
@@ -0,0 +1,12 @@
+package edge.project3.authentication.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String username;
+	private String password;
+	private String code;
+
+}
