726b0fd3a1160a8ce2aeb75aa7943a81286594b2
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-12 08:33:11 +0200

    Register clients with the authorization server

--- AuthorizationServerOAuth2/src/main/java/edge/authserver/security/AuthServerConfig.java
+++ AuthorizationServerOAuth2/src/main/java/edge/authserver/security/AuthServerConfig.java
@@ -3,6 +3,7 @@ package edge.authserver.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -26,5 +27,36 @@ public class AuthServerConfig
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
 		endpoints.authenticationManager(authenticationManager);
 	}
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients)
+				throws Exception {
+
+//		// 1.
+//		// Creates an instance using the ClientDetailsService implementation
+//		var service = new InMemoryClientDetailsService();
+//		
+//		// Creates an instance of ClientDetails and sets the needed details about the client
+//		var client = new BaseClientDetails();
+//		client.setClientId("client");
+//		client.setClientSecret("secret");
+//		client.setScope(List.of("read"));
+//		client.setAuthorizedGrantTypes(List.of("password"));
+//		
+//		// Adds the ClientDetails instance to InMemoryClientDetailsService
+//		service.setClientDetailsStore(Map.of("client", client));
+//		
+//		// Configures ClientDetailsService for use by our authorization server
+//		clients.withClientDetails(service);
+		
+		// 2.
+		clients.inMemory()
+				.withClient("client")
+				.secret("secret")
+				.authorizedGrantTypes("password", "refresh_token")
+				.scopes("read");
+		
+		
+	}	
 	
 }
