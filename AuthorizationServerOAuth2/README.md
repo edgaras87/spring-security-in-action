1bd350c66611aaa73b83e785f1a3ea85e5cab432
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-12 08:24:03 +0200

    Minimal configuration for authorization server

--- AuthorizationServerOAuth2/README.md
+++ AuthorizationServerOAuth2/README.md
@@ -1,2 +1,12 @@
 # Notes
 
+## Authorization Server
+Spring Security OAuth2 Boot simplifies standing up an OAuth 2.0 Authorization Server.
+https://docs.spring.io/spring-security-oauth2-boot/docs/2.2.6.BUILD-SNAPSHOT/reference/htmlsingle/
+
+
+## Spring Cloud
+Spring Boot compatibility
+https://spring.io/projects/spring-cloud
+dependencyManagement
+https://reddit.fun/19436/beancreationexception-creating-configurationpropertiesbeans
---
+++ AuthorizationServerOAuth2/src/main/java/edge/authserver/security/AuthServerConfig.java
@@ -0,0 +1,13 @@
+package edge.authserver.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig 
+		extends AuthorizationServerConfigurerAdapter {
+
+}
