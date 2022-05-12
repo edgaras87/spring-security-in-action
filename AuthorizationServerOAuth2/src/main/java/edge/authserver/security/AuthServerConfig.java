a1e403e806fe5b60c7d4fde7d70db5eb84c806b9
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-12 08:26:57 +0200

    Define user management

--- AuthorizationServerOAuth2/src/main/java/edge/authserver/security/AuthServerConfig.java
+++ AuthorizationServerOAuth2/src/main/java/edge/authserver/security/AuthServerConfig.java
@@ -1,13 +1,30 @@
 package edge.authserver.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 
+/**
+ * @EnableAuthorizationServer - Instruct Spring Boot to enable the configuration 
+ * specific to the OAuth 2 authorization server. We can customize this configuration 
+ * by extending the AuthorizationServerConfigurerAdapter class and overriding 
+ * specific methods.
+ */
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig 
 		extends AuthorizationServerConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.authenticationManager(authenticationManager);
+	}
+	
 }
---
+++ AuthorizationServerOAuth2/src/main/java/edge/authserver/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package edge.authserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig  
+	extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		var manager = new InMemoryUserDetailsManager();
+		var user = User.withUsername("jon")
+						.password("pass")
+						.authorities("read")
+						.build();
+		manager.createUser(user);
+		return manager;
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	// Adds the AuthenticationManager instance as a bean in the Spring context
+	@Bean
+    @Override
+	public AuthenticationManager authenticationManagerBean() 
+			throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+}
