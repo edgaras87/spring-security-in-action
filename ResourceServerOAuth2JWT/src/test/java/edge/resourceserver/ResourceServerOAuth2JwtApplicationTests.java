530ebe5d0e5738f376e6e76cb48af0359a3588e8
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-14 10:45:10 +0200

    Initialize resource server

---
+++ ResourceServerOAuth2JWT/src/test/java/edge/resourceserver/ResourceServerOAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package edge.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerOAuth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
