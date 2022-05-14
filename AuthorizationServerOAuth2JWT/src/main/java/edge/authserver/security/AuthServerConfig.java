6def72c2a71d746c6812dd4da48f166540caeb61
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-14 14:25:25 +0200

    Implementing signing/validation of JWTs with symmetric and asymmetric keys

--- AuthorizationServerOAuth2JWT/src/main/java/edge/authserver/security/AuthServerConfig.java
+++ AuthorizationServerOAuth2JWT/src/main/java/edge/authserver/security/AuthServerConfig.java
@@ -1,9 +1,12 @@
 package edge.authserver.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -12,6 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,6 +23,18 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Value("${jwt.symmetric.key}")
+	private String jwtSymmetricKey;
+	
+    @Value("${jwt.asymmetric.password}")
+    private String password;
+
+    @Value("${jwt.asymmetric.privateKey}")
+    private String privateAsymmetricKey;
+
+    @Value("${jwt.asymmetric.alias}")
+    private String alias;
 	
 	// autowiring jwtAccessTokenConverter because of different implementation
 	// when one implementation is used there is no need of this autowiring
@@ -47,10 +63,31 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         return new JwtTokenStore(jwtAccessTokenConverter);
     }
 
-    @Bean
+    //@Bean
     public JwtAccessTokenConverter jwtAccessTokenConverterDummy() {
         var converter = new JwtAccessTokenConverter();
         return converter;
+    }
+    
+    @Profile("symmetric")
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterSymmetric() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtSymmetricKey);
+        return converter;
+    }
+    
+    @Profile("asymmetric")
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterAsymmetric() {
+        var converter = new JwtAccessTokenConverter();
+        KeyStoreKeyFactory keyStoreKeyFactory =
+                new KeyStoreKeyFactory(
+                        new ClassPathResource(privateAsymmetricKey),
+                        password.toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
+        return converter;
+        
     }
 
 }
--- ResourceServerOAuth2JWT/src/main/java/edge/resourceserver/security/ResourceServerConfig.java
+++ ResourceServerOAuth2JWT/src/main/java/edge/resourceserver/security/ResourceServerConfig.java
@@ -1,9 +1,11 @@
 package edge.resourceserver.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -14,6 +16,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Value("${jwt.symmetric.key}")
+	private String jwtSymmetricKey;
+	
+    @Value("${jwt.asymmetric.publicKey}")
+    private String publicAsymmetricKey;
 	
 	// autowiring jwtAccessTokenConverter because of different implementation
 	// when one implementation is used there is no need of this autowiring
@@ -31,9 +39,25 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         return new JwtTokenStore(jwtAccessTokenConverter);
     }
 
-    @Bean
+    //@Bean
     public JwtAccessTokenConverter jwtAccessTokenConverterDummy() {
         var converter = new JwtAccessTokenConverter();
+        return converter;
+    }
+    
+    @Profile("symmetric")
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterSymmetric() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtSymmetricKey);
+        return converter;
+    }
+    
+    @Profile("asymmetric")
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterAsymmetric() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setVerifierKey(publicAsymmetricKey);
         return converter;
     }
     
