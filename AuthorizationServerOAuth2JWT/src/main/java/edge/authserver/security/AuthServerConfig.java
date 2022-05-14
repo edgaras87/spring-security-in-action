5dd738714b4de200e20c54799beda89715c111e0
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-14 11:35:30 +0200

    Define carcass of JWT usage between servers without specific implementation
    Define carcass for JWTs issue and their inspection without specific logic. For specific logic, that is semmetric or asymmetric keys implementation, this defined carcass will be the same.

--- AuthorizationServerOAuth2JWT/src/main/java/edge/authserver/security/AuthServerConfig.java
+++ AuthorizationServerOAuth2JWT/src/main/java/edge/authserver/security/AuthServerConfig.java
@@ -1,12 +1,17 @@
 package edge.authserver.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -14,6 +19,12 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	// autowiring jwtAccessTokenConverter because of different implementation
+	// when one implementation is used there is no need of this autowiring
+	@Autowired
+	@Lazy
+	public JwtAccessTokenConverter jwtAccessTokenConverter;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -26,7 +37,20 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints.authenticationManager(authenticationManager)
+        		 .tokenStore(tokenStore())
+        		 .accessTokenConverter(jwtAccessTokenConverter);		
 	}
+	
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterDummy() {
+        var converter = new JwtAccessTokenConverter();
+        return converter;
+    }
 
 }
--- ResourceServerOAuth2JWT/src/main/java/edge/resourceserver/security/ResourceServerConfig.java
+++ ResourceServerOAuth2JWT/src/main/java/edge/resourceserver/security/ResourceServerConfig.java
@@ -1,11 +1,40 @@
 package edge.resourceserver.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	// autowiring jwtAccessTokenConverter because of different implementation
+	// when one implementation is used there is no need of this autowiring
+	@Autowired
+	@Lazy
+	public JwtAccessTokenConverter jwtAccessTokenConverter;
 
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore());
+    }
 
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverterDummy() {
+        var converter = new JwtAccessTokenConverter();
+        return converter;
+    }
+    
 }
