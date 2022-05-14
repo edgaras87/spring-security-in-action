bc2e7107c570cc385e1aa65f32d2dc16efce4b1b
edgaras <stasiukaitisedgaras87@gmail.com>
2022-05-14 11:07:04 +0200

    Configure resource server

---
+++ ResourceServerOAuth2JWT/src/main/java/edge/resourceserver/security/ResourceServerConfig.java
@@ -0,0 +1,11 @@
+package edge.resourceserver.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig {
+
+
+}
